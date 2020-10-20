64e27893663c3e1fda59735cd07d9f6f6c8e5576
Tyler Hilbert <TylerJoshuaHilbert@gmail.com>
2020-10-20 15:18:05 -0400

    Plot

--- Homework2/Plot.py
+++ Homework2/Plot.py
@@ -1,11 +1,23 @@
 import csv
-import sys
+import matplotlib.pyplot as plt
+
 
 f = open("data.csv", 'rt')
 
 reader = csv.reader(f)
-data = []
+dataX1 = []
+dataY1 = []
+dataXN = []
+dataYN = []
 for row in reader:
-    data.append( (row[1], row[2]) )
+    if '-1' in row[0]:
+        dataXN.append(float(row[1]))
+        dataYN.append(float(row[2]))
+    else:
+        dataX1.append(float(row[1]))
+        dataY1.append(float(row[2]))
 
-print (data)
+
+plt.plot(dataX1, dataY1, 'ro')
+plt.plot(dataXN, dataYN, 'bo')
+plt.show()
