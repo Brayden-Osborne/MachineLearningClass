969c578c2e2276c39a5648eb06b7512d8ff06a2c
Matthew <terrymv#mail.com>
2020-11-24 17:49:49 -0500

    my commit

---
+++ deltaRule.py
@@ -0,0 +1,26 @@
+import numpy as np
+from mpl_toolkits import mplot3d
+import matplotlib.pyplot as plt
+
+def deltaRule():
+    pass
+
+def getData(batch):
+
+    return x1, x2, y
+    pass
+
+def runModel(x1,x2,w1,w2):
+    pass
+
+if __name__ == "__main__":
+    x1, x2, y  = getData(1)
+    w1 = 10; w2 = 15
+    Error = []
+    lr = 0.01
+
+    while True:
+        # Run the model
+        # get Error
+        # perform Delta rule and update weights
+    
